b1ffd58a7adb3e03e09711e7218ec6c5b7ad073f
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-12-04 23:27:34 +0500

    change

--- users/api/views.py
+++ users/api/views.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView, UpdateAPIView
-from rest_framework.views import APIView
 from users.serializers import UserSerializer, ChangePasswordSerializer
 
 User = get_user_model()
